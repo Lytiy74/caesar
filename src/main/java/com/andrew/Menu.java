1412baeeb47347b70ba5fd1c01075ec7a4e5b5ad
Lytiy <lytiy7404@gmail.com>
2023-10-03 23:08:47 +0300

    reworked Menu.mainmenu()

--- src/main/java/com/andrew/Menu.java
+++ src/main/java/com/andrew/Menu.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class Menu {
     static protected void  mainmenu(){
-        Scanner consoleInput = new Scanner(System.in);
+        Scanner consoleScanner = new Scanner(System.in);
         int optionIndex = 1;
+        String consoleInput;
+        int userOption = -1;
         HashMap<Integer, String> menuOptions = new HashMap<>();
         for (MenuElements elements : MenuElements.values()) {
             menuOptions.put(optionIndex++, elements.getTitle());
@@ -14,6 +16,16 @@ public class Menu {
         for (Map.Entry<Integer, String> option : menuOptions.entrySet()) {
             System.out.printf("%d. %s\n", option.getKey(), option.getValue());
         }
+        do {
+            consoleInput = consoleScanner.nextLine();
+            for (Map.Entry<Integer, String> option : menuOptions.entrySet()) {
+                if (consoleInput.equals(Integer.toString(option.getKey())) || consoleInput.equals(option.getValue())) {
+                    userOption = option.getKey();
+                    break;
+                }
+            }
+        }while(userOption == -1);
+
 
     }
 
