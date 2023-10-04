c2b8ab070ce66d30f9a0c59ee58fccd6733523af
Lytiy <lytiy7404@gmail.com>
2023-10-04 20:22:05 +0300

    reworked Menu.mainmenu()

--- src/main/java/com/andrew/Main.java
+++ src/main/java/com/andrew/Main.java
@@ -2,9 +2,11 @@ package com.andrew;
 
 public class Main {
     public static void main(String[] args) {
+        //Якщо програма запутилася без параметрів - викликаємо клас Menu з перегруженим методом mainmenu() який буде приймати від користувача дані.
         if (args.length == 0) {
             Menu.mainmenu();
         }
+        //Якщо програма запустилася з параметрами то передаємо ці параметри до меню.
         else
             Menu.mainmenu(args);
     }
--- src/main/java/com/andrew/Menu.java
+++ src/main/java/com/andrew/Menu.java
@@ -1,27 +1,40 @@
 package com.andrew;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 
 public class Menu {
+    private static int optionIndex = 1;
+    private static int userOption = -1;
+    private final static HashMap<Integer, String> menuOptions = new HashMap<>();
     static protected void  mainmenu(){
         Scanner consoleScanner = new Scanner(System.in);
-        int optionIndex = 1;
         String consoleInput;
-        int userOption = -1;
-        HashMap<Integer, String> menuOptions = new HashMap<>();
+
+        //Додаємо всі елементи множини MenuElements в колекцію MenuOptions.
         for (MenuElements elements : MenuElements.values()) {
             menuOptions.put(optionIndex++, elements.getTitle());
         }
-        System.out.println("Choose an option:");
-        for (Map.Entry<Integer, String> option : menuOptions.entrySet()) {
-            System.out.printf("%d. %s\n", option.getKey(), option.getValue());
-        }
         do {
-            consoleInput = consoleScanner.nextLine();
+            //Виводимо в консоль всі елементи меню з мапи.
+            System.out.println("Choose an option:");
             for (Map.Entry<Integer, String> option : menuOptions.entrySet()) {
-                if (consoleInput.equals(Integer.toString(option.getKey())) || consoleInput.equals(option.getValue())) {
-                    userOption = option.getKey();
-                    break;
+                System.out.printf("%d. %s\n", option.getKey(), option.getValue());
+            }
+
+            //Якщо користувач ввів в консоль числове значення(тобто ключ) перевіряємо чи є цей ключ в колекції і якщо істина то присвоюємо це в поле userOption.
+            if (consoleScanner.hasNextInt()){
+                consoleInput = consoleScanner.nextLine();
+                if (menuOptions.containsKey(Integer.parseInt(consoleInput)))
+                    userOption = Integer.parseInt(consoleInput);
+            }
+            //У випадку якщо користувач ввів текстовий варіант меню. Перебираємо всю колекцію і присвоюємо ключ до поля userOption
+            else{
+                consoleInput = consoleScanner.nextLine();
+                for (Map.Entry<Integer, String> option:menuOptions.entrySet()) {
+                    if (consoleInput.equals(option.getValue()))
+                        userOption = option.getKey();
                 }
             }
         }while(userOption == -1);
@@ -30,12 +43,13 @@ public class Menu {
     }
 
     static protected void mainmenu(String[] args){
-        //will add menu
+        //Згідно умови отримуємо аргументи типу command filePath key.
+        
     }
 
 }
 
- enum MenuElements {
+ enum MenuElements { // Поле з елементами меню
     ENCRYPT ("Encrypt"),
     DECRYPT("Decrypt"),
     BRUT_FORCE("BRUT_FORCE"),
